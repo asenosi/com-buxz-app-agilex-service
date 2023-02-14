97b599cc5aa626323e5729fd26efa0722ad828c0
Aobakwe Senosi <senosiag@gmail.com>
2023-02-15 01:00:33 +0200

    Journey of 100 commits, start here

---
+++ src/main/java/com/buxz/app/agilexservice/AgilexServiceApplication.java
@@ -0,0 +1,13 @@
+package com.buxz.app.agilexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgilexServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buxz/app/agilexservice/AgilexServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buxz.app.agilexservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgilexServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
